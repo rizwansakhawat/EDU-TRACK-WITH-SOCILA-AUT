e900241eea5675178a4f1bbb7f6543ab82273e0f
Muhammad Rizwan <rizwansakhawat111@gmail.com>
2026-03-05 16:32:44 +0500

    make user serializer, viewset

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User, Organization
+from rest_framework.exceptions import ValidationError
+
     
     
 class OrganizationsSerializer(serializers.ModelSerializer):
@@ -11,15 +13,34 @@ class OrganizationsSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    organization = serializers.SlugRelatedField( slug_field='name', queryset=Organization.objects.all())
-    
-
+    organization = serializers.SlugRelatedField( slug_field='name',
+    queryset=Organization.objects.all(),
+     required=False, allow_null=True )
     class Meta:
         model = User
-        fields = ['username', 'password', 'role', 'organization']
+        fields = ['id', 'username', 'password', 'role', 'organization']
+        
+    def validate(self, data):
+        # Access the user making the request
+        request_user = self.context['request'].user
+        target_role = data.get('role')
+        target_org = data.get('organization')
+
+        # 1. Role Protection: Only SUPERADMIN can create another SUPERADMIN
+        if target_role == "SUPERADMIN" and request_user.role != "SUPERADMIN":
+            raise ValidationError({"role": "Only SuperAdmins can assign the SuperAdmin role."})
+        
+        if request_user.role == "ORGADMIN":
+            # Force the organization to be the same as the creator
+            data['organization'] = request_user.organization
+            
+            # Prevent ORGADMIN from trying to pick another org
+            if target_org and target_org != request_user.organization:
+                raise ValidationError({"organization": "You cannot create users for other organizations."})
+
+        return data
+
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
     
-        
-        
--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,10 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsInstructor, IsOrgAdmin, IsStudent, IsSuperAdmin
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import ValidationError
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
 
 from .models import User, Organization
 from .serializers import UserSerializer, OrganizationsSerializer
@@ -10,30 +14,69 @@ from .serializers import UserSerializer, OrganizationsSerializer
 # Create your views here.
 
 class UserViewset(viewsets.ModelViewSet):
-    # queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['organization']
+    search_fields = ['username', 'email']
     
     def get_queryset(self):
         user = self.request.user
         if user.role == "SUPERADMIN":
             return User.objects.all()
-        
         if user.role == "ORGADMIN":
-            return User.objects.filter
-        
-        return User.objects.filter(id=User.id)
+            return User.objects.filter(organization=user.organization)
+        return User.objects.filter(id=user.id)
     
     def get_permissions(self):
         
         if self.action == 'create':
-            return [IsAuthenticated(), IsOrgAdmin()]
-        if self.action in ["update", "partial_update"]:
-            return [IsAuthenticated(), IsOrgAdmin()]
-        if self.action == "destroy":
-            return [IsAuthenticated(), IsSuperAdmin()]
+            permission_classes = [IsAuthenticated , (IsOrgAdmin | IsSuperAdmin)]
+        elif self.action in ["update", "partial_update"]:
+            permission_classes =  [IsAuthenticated, IsOrgAdmin]
+        elif self.action == "destroy":
+            permission_classes = [IsAuthenticated, IsSuperAdmin]
+        else:
+            permission_classes = [IsAuthenticated]
+    
+        return [permission() for permission in permission_classes]
+    
+    
+    # def perform_create(self, serializer):
+    #     current_user = self.request.user
         
-        return [IsAuthenticated()]
+    #     target_role = self.request.data.get('role')
+    #     if target_role == "SUPERADMIN" and current_user.role != "SUPERADMIN":
+    #         raise ValidationError({"role":"You do not have permission to assign the SUPERADMIN role."})
+            
+    #     if current_user.role == "ORGADMIN":
+    #         serializer.save(organization=current_user.organization)
+    #     else:
+    #         serializer.save()
+        
+        
+        
+    
+        
+        
+    
+    # #  Object-Level Protection --- define in serializer--******
+    # def perform_update(self, serializer):
+    #     instance = self.get_object()
+
+    #     if (
+    #         self.request.user.role == "ORGADMIN" and
+    #         instance.organization != self.request.user.organization
+    #     ):
+    #         raise PermissionDenied("Cannot modify another organization user")
+
+    #     serializer.save()
+
+    # def perform_destroy(self, instance):
+    #     if self.request.user.role != "SUPERADMIN":
+    #         raise PermissionDenied("Only SuperAdmin can delete users")
+
+    #     instance.delete()
     
      
         
